059bd1e1c3c2d5cffc3c121c569808d9952a951d
manzanofp <felipemanzanofp@gmail.com>
2023-01-04 15:17:50 -0300

    Initial project

---
+++ src/main/java/com/manzanofp/mzndelivery/MzndeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.manzanofp.mzndelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzndeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzndeliveryApplication.class, args);
+	}
+
+}
